df9e307448559cb10e257971a9a7d311a7c33c3f
Fish-bowl <symbolt.ap@gmail.com>
2019-03-04 15:59:13 -0700

    initial commit

---
+++ fest-test/screens/ChatScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+class ChatScreen extends React.Component {
+  render() {
+    return(
+      <View >
+        <Text>Hi</Text>
+      </View>
+    )
+  }
+}
+
+export default ChatScreen
